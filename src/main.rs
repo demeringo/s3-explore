41a9329afa4d8d783e1b1f44f3fc452c9e48e709
demeringo <demeringo@gmail.com>
2025-06-16 09:38:14 +0000

    wip: list bucket example from aws

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,105 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_s3::{Client, config::Region, types::BucketLocationConstraint};
+
+#[tokio::main]
+async fn main() -> Result<(), S3ExampleError> {
+    let region_provider = RegionProviderChain::first_try(Region::new("us-west-2"));
+    let region = region_provider.region().await.unwrap();
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
+
+    let bucket_region = BucketLocationConstraint::try_parse(&region.to_string())
+        .map_err(|e| S3ExampleError::new(format!("{e:?}")))?;
+
+    if let Err(e) = show_buckets(true, &client, bucket_region).await {
+        eprintln!("{:?}", e);
+    };
+
+    Ok(())
+}
+
+// Shows your buckets, or those just in the region.
+// snippet-start:[s3.rust.list_buckets]
+async fn show_buckets(
+    strict: bool,
+    client: &Client,
+    region: BucketLocationConstraint,
+) -> Result<(), S3ExampleError> {
+    let mut buckets = client.list_buckets().into_paginator().send();
+
+    let mut num_buckets = 0;
+    let mut in_region = 0;
+
+    while let Some(Ok(output)) = buckets.next().await {
+        for bucket in output.buckets() {
+            num_buckets += 1;
+            if strict {
+                let r = client
+                    .get_bucket_location()
+                    .bucket(bucket.name().unwrap_or_default())
+                    .send()
+                    .await?;
+
+                if r.location_constraint() == Some(&region) {
+                    println!("{}", bucket.name().unwrap_or_default());
+                    in_region += 1;
+                }
+            } else {
+                println!("{}", bucket.name().unwrap_or_default());
+            }
+        }
+    }
+
+    println!();
+    if strict {
+        println!(
+            "Found {} buckets in the {} region out of a total of {} buckets.",
+            in_region, region, num_buckets
+        );
+    } else {
+        println!("Found {} buckets in all regions.", num_buckets);
+    }
+
+    Ok(())
+}
+// snippet-end:[s3.rust.list_buckets]
+
+/// S3ExampleError provides a From<T: ProvideErrorMetadata> impl to extract
+/// client-specific error details. This serves as a consistent backup to handling
+/// specific service errors, depending on what is needed by the scenario.
+/// It is used throughout the code examples for the AWS SDK for Rust.
+#[derive(Debug)]
+pub struct S3ExampleError(String);
+impl S3ExampleError {
+    pub fn new(value: impl Into<String>) -> Self {
+        S3ExampleError(value.into())
+    }
+
+    pub fn add_message(self, message: impl Into<String>) -> Self {
+        S3ExampleError(format!("{}: {}", message.into(), self.0))
+    }
+}
+
+impl<T: aws_sdk_s3::error::ProvideErrorMetadata> From<T> for S3ExampleError {
+    fn from(value: T) -> Self {
+        S3ExampleError(format!(
+            "{}: {}",
+            value
+                .code()
+                .map(String::from)
+                .unwrap_or("unknown code".into()),
+            value
+                .message()
+                .map(String::from)
+                .unwrap_or("missing reason".into()),
+        ))
+    }
+}
+
+impl std::error::Error for S3ExampleError {}
+
+impl std::fmt::Display for S3ExampleError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
